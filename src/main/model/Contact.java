3feab0576d93acc94b3e160c142363acb49eab5e
jenessagordon <68665601+jenessagordon@users.noreply.github.com>
2022-07-23 11:59:19 -0700

    Project 0 - Reduced some duplication in tests, implemented Contact

--- src/main/model/Contact.java
+++ src/main/model/Contact.java
@@ -1,55 +1,85 @@
 package model;
 
+// Represents a contact containing the contact’s full name, address, phone number, email, and birthday, along with the
+// date the user added the contact to the contact list, where the user met the contact, and what Category the contact
+// is in. Category represents the type of the contact, and is one of: FAMILY, FRIEND, WORK, or OTHER.
 public class Contact {
+    String fullName;
+    String address;
+    String phoneNum;
+    String email;
+    String birthday;
+    String dateAdded;
+    String locationMet;
+    Category category;
 
     public enum Category {
         FAMILY, FRIEND, WORK, OTHER
     }
 
     // EFFECTS: creates a Contact with the given attributes
-    public Contact(String fullName, String address,
-                   String phoneNum, String email,
-                   String birthday, String dateAdded,
-                   String locationMet, Category category) {
-        // stub
+    public Contact(String fullName,
+                   String address,
+                   String phoneNum,
+                   String email,
+                   String birthday,
+                   String dateAdded,
+                   String locationMet,
+                   Category category) {
+        this.fullName = fullName;
+        this.address = address;
+        this.phoneNum = phoneNum;
+        this.email = email;
+        this.birthday = birthday;
+        this.dateAdded = dateAdded;
+        this.locationMet = locationMet;
+        this.category = category;
     }
 
     // EFFECTS: returns this as a String of the form “Full name: " + fullName + “\n" +
     // “Address: " + address + “\n" + … + “Category: " + category + "\n" +
     // “-------------------"
     public String contactToString() {
-        return null; // stub
+        return "Full name: " + fullName + "\n"
+                + "Address: " + address + "\n"
+                + "Phone number: " + phoneNum + "\n"
+                + "Email: " + email + "\n"
+                + "Birthday: " + birthday + "\n"
+                + "Date added: " + dateAdded + "\n"
+                + "Location met: " + locationMet + "\n"
+                + "Category: " + category + "\n"
+                + "-------------------";
     }
 
     public String getFullName() {
-        return null; // stub
+        return fullName;
     }
 
     public String getAddress() {
-        return null; // stub
+        return address;
     }
 
     public String getPhoneNum() {
-        return null; // stub
+        return phoneNum;
     }
 
     public String getEmail() {
-        return null; // stub
+        return email;
     }
 
     public String getBirthday() {
-        return null; // stub
+        return birthday;
     }
 
     public String getDateAdded() {
-        return null; // stub
+        return dateAdded;
     }
 
     public String getLocationMet() {
-        return null; // stub
+        return locationMet;
     }
 
     public Category getCategory() {
-        return null; // stub
+        return category;
     }
 }
--- src/test/model/TestContactList.java
+++ src/test/model/TestContactList.java
@@ -13,7 +13,10 @@ public class TestContactList {
     private Contact testContact3;
     private Contact testContact4;
     private Contact testContact5;
-    ArrayList<Contact> testListOfContacts = new ArrayList<>();
+    private ArrayList<Contact> testListOfContacts = new ArrayList<>();
+    private String testListOfContactsToStringResult;
+    private String testContact3ContactToStringResult;
+    private String testContact4ContactToStringResult;
 
     // TODO: reduce duplication in this class
 
@@ -65,6 +68,69 @@ public class TestContactList {
                 "October 16, 2007",
                 "",
                 Contact.Category.FAMILY);
+        testListOfContactsToStringResult = "\n" + "Full name: " + "Pete Hamilton" + "\n"
+                + "Address: " + "21 Throllalach Way, Vancouver, BC, N2D 7H1" + "\n"
+                + "Phone number: " + "(403) 893-2343" + "\n"
+                + "Email: " + "peteham@hotmail.com" + "\n"
+                + "Birthday: " + "July 7, 1970" + "\n"
+                + "Date added: " + "October 16, 2007" + "\n"
+                + "Location met: " + "" + "\n"
+                + "Category: " + Contact.Category.FAMILY + "\n"
+                + "-------------------"
+                + "\n" + "Full name: " + "Molly Brown" + "\n"
+                + "Address: " + "178 Turner Road, Vancouver, BC, V8L 9E2" + "\n"
+                + "Phone number: " + "(604) 902-3789" + "\n"
+                + "Email: " + "mollybrown@gmail.com" + "\n"
+                + "Birthday: " + "January 9, 2000" + "\n"
+                + "Date added: " + "July 21, 2022" + "\n"
+                + "Location met: " + "Walter Gage" + "\n"
+                + "Category: " + Contact.Category.FRIEND + "\n"
+                + "-------------------"
+                + "\n" + "Full name: " + "Bob Li" + "\n"
+                + "Address: " + "1345 Blankar Terrace, Edmonton, AB, H7T 9P4" + "\n"
+                + "Phone number: " + "(403) 124-6784" + "\n"
+                + "Email: " + "bobli@gmail.com" + "\n"
+                + "Birthday: " + "November 16, 1999" + "\n"
+                + "Date added: " + "May 1, 2022" + "\n"
+                + "Location met: " + "Business conference" + "\n"
+                + "Category: " + Contact.Category.WORK + "\n"
+                + "-------------------"
+                + "\n" + "Full name: " + "Julie Allo" + "\n"
+                + "Address: " + "4 Ruffert Street, Kelowna, BC, A5T 2Y6" + "\n"
+                + "Phone number: " + "(604) 854-4397" + "\n"
+                + "Email: " + "julieallow@gmail.com" + "\n"
+                + "Birthday: " + "" + "\n"
+                + "Date added: " + "June 3, 2021" + "\n"
+                + "Location met: " + "The hospital" + "\n"
+                + "Category: " + Contact.Category.OTHER + "\n"
+                + "-------------------"
+                + "\n" + "Full name: " + "" + "\n"
+                + "Address: " + "56 Singer Crescent, Canmore, AB, R3E 8I4" + "\n"
+                + "Phone number: " + "(403) 223-3421" + "\n"
+                + "Email: " + "hair@thehairplace.com" + "\n"
+                + "Birthday: " + "" + "\n"
+                + "Date added: " + "March 27, 2021" + "\n"
+                + "Location met: " + "" + "\n"
+                + "Category: " + Contact.Category.OTHER + "\n"
+                + "-------------------";
+        testContact3ContactToStringResult = "Full name: " + "Julie Allo" + "\n"
+                + "Address: " + "4 Ruffert Street, Kelowna, BC, A5T 2Y6" + "\n"
+                + "Phone number: " + "(604) 854-4397" + "\n"
+                + "Email: " + "julieallow@gmail.com" + "\n"
+                + "Birthday: " + "" + "\n"
+                + "Date added: " + "June 3, 2021" + "\n"
+                + "Location met: " + "The hospital" + "\n"
+                + "Category: " + Contact.Category.OTHER + "\n"
+                + "-------------------";
+        testContact4ContactToStringResult = "Full name: " + "" + "\n"
+                + "Address: " + "56 Singer Crescent, Canmore, AB, R3E 8I4" + "\n"
+                + "Phone number: " + "(403) 223-3421" + "\n"
+                + "Email: " + "hair@thehairplace.com" + "\n"
+                + "Birthday: " + "" + "\n"
+                + "Date added: " + "March 27, 2021" + "\n"
+                + "Location met: " + "" + "\n"
+                + "Category: " + Contact.Category.OTHER + "\n"
+                + "-------------------";
     }
 
     @Test
@@ -157,9 +223,97 @@ public class TestContactList {
         assertEquals(testListOfContacts, testContactList.getListOfContacts());
     }
 
-    // TODO: listofContactsToString test(s)
+    @Test
+    public void testListOfContactsToStringInOrder() {
+        testContactList.addContact("Pete Hamilton",
+                "21 Throllalach Way, Vancouver, BC, N2D 7H1",
+                "(403) 893-2343",
+                "peteham@hotmail.com",
+                "July 7, 1970",
+                "October 16, 2007",
+                "",
+                Contact.Category.FAMILY);
+        testContactList.addContact("Molly Brown",
+                "178 Turner Road, Vancouver, BC, V8L 9E2",
+                "(604) 902-3789",
+                "mollybrown@gmail.com",
+                "January 9, 2000",
+                "July 21, 2022",
+                "Walter Gage",
+                Contact.Category.FRIEND);
+        testContactList.addContact("Bob Li",
+                "1345 Blankar Terrace, Edmonton, AB, H7T 9P4",
+                "(403) 124-6784",
+                "bobli@gmail.com",
+                "November 16, 1999",
+                "May 1, 2022",
+                "Business conference",
+                Contact.Category.WORK);
+        testContactList.addContact("Julie Allo",
+                "4 Ruffert Street, Kelowna, BC, A5T 2Y6",
+                "(604) 854-4397",
+                "julieallow@gmail.com",
+                "",
+                "June 3, 2021",
+                "The hospital",
+                Contact.Category.OTHER);
+        testContactList.addContact("",
+                "56 Singer Crescent, Canmore, AB, R3E 8I4",
+                "(403) 223-3421",
+                "hair@thehairplace.com",
+                "",
+                "March 27, 2021",
+                "",
+                Contact.Category.OTHER);
+        assertEquals(testListOfContactsToStringResult,
+                testContactList.listOfContactsToString());
+    }
 
-
+    @Test
+    public void testListOfContactsToStringOutOfOrder() {
+        testContactList.addContact("Molly Brown",
+                "178 Turner Road, Vancouver, BC, V8L 9E2",
+                "(604) 902-3789",
+                "mollybrown@gmail.com",
+                "January 9, 2000",
+                "July 21, 2022",
+                "Walter Gage",
+                Contact.Category.FRIEND);
+        testContactList.addContact("Julie Allo",
+                "4 Ruffert Street, Kelowna, BC, A5T 2Y6",
+                "(604) 854-4397",
+                "julieallow@gmail.com",
+                "",
+                "June 3, 2021",
+                "The hospital",
+                Contact.Category.OTHER);
+        testContactList.addContact("",
+                "56 Singer Crescent, Canmore, AB, R3E 8I4",
+                "(403) 223-3421",
+                "hair@thehairplace.com",
+                "",
+                "March 27, 2021",
+                "",
+                Contact.Category.OTHER);
+        testContactList.addContact("Pete Hamilton",
+                "21 Throllalach Way, Vancouver, BC, N2D 7H1",
+                "(403) 893-2343",
+                "peteham@hotmail.com",
+                "July 7, 1970",
+                "October 16, 2007",
+                "",
+                Contact.Category.FAMILY);
+        testContactList.addContact("Bob Li",
+                "1345 Blankar Terrace, Edmonton, AB, H7T 9P4",
+                "(403) 124-6784",
+                "bobli@gmail.com",
+                "November 16, 1999",
+                "May 1, 2022",
+                "Business conference",
+                Contact.Category.WORK);
+        assertEquals(testListOfContactsToStringResult,
+                testContactList.listOfContactsToString());
+    }
 
     @Test
     public void testContactsOfCategoryToStringReturnEmptyString() {
@@ -208,15 +362,7 @@ public class TestContactList {
                 "June 3, 2021",
                 "The hospital",
                 Contact.Category.OTHER);
-        assertEquals("\n" + "Full name: " + "Julie Allo" + "\n" +
-                "Address: " + "4 Ruffert Street, Kelowna, BC, A5T 2Y6" + "\n" +
-                "Phone number: " + "(604) 854-4397" + "\n" +
-                "Email: " + "julieallow@gmail.com" + "\n" +
-                "Birthday: " + "" + "\n" +
-                "Date added: " + "June 3, 2021" + "\n" +
-                "Location met: " + "The hospital" + "\n" +
-                "Category: " + Contact.Category.OTHER + "\n" +
-                "-------------------",
+        assertEquals("\n" + testContact3ContactToStringResult,
                 testContactList.contactsOfCategoryToString(Contact.Category.OTHER));
     }
 
@@ -254,204 +400,8 @@ public class TestContactList {
                 "March 27, 2021",
                 "",
                 Contact.Category.OTHER);
-        assertEquals("\n" + "Full name: " + "Julie Allo" + "\n" +
-                "Address: " + "4 Ruffert Street, Kelowna, BC, A5T 2Y6" + "\n" +
-                "Phone number: " + "(604) 854-4397" + "\n" +
-                "Email: " + "julieallow@gmail.com" + "\n" +
-                "Birthday: " + "" + "\n" +
-                "Date added: " + "June 3, 2021" + "\n" +
-                "Location met: " + "The hospital" + "\n" +
-                "Category: " + Contact.Category.OTHER + "\n" +
-                "-------------------" +
-                "\n" + "Full name: " + "" + "\n" +
-                "Address: " + "56 Singer Crescent, Canmore, AB, R3E 8I4" + "\n" +
-                "Phone number: " + "(403) 223-3421" + "\n" +
-                "Email: " + "hair@thehairplace.com" + "\n" +
-                "Birthday: " + "" + "\n" +
-                "Date added: " + "March 27, 2021" + "\n" +
-                "Location met: " + "" + "\n" +
-                "Category: " + Contact.Category.OTHER + "\n" +
-                "-------------------",
+        assertEquals("\n" + testContact3ContactToStringResult
+                        + "\n" + testContact4ContactToStringResult,
                 testContactList.contactsOfCategoryToString(Contact.Category.OTHER));
-    }
-
-    @Test
-    public void testListOfContactsToStringInOrder() {
-        testContactList.addContact("Pete Hamilton",
-                "21 Throllalach Way, Vancouver, BC, N2D 7H1",
-                "(403) 893-2343",
-                "peteham@hotmail.com",
-                "July 7, 1970",
-                "October 16, 2007",
-                "",
-                Contact.Category.FAMILY);
-        testContactList.addContact("Molly Brown",
-                "178 Turner Road, Vancouver, BC, V8L 9E2",
-                "(604) 902-3789",
-                "mollybrown@gmail.com",
-                "January 9, 2000",
-                "July 21, 2022",
-                "Walter Gage",
-                Contact.Category.FRIEND);
-        testContactList.addContact("Bob Li",
-                "1345 Blankar Terrace, Edmonton, AB, H7T 9P4",
-                "(403) 124-6784",
-                "bobli@gmail.com",
-                "November 16, 1999",
-                "May 1, 2022",
-                "Business conference",
-                Contact.Category.WORK);
-        testContactList.addContact("Julie Allo",
-                "4 Ruffert Street, Kelowna, BC, A5T 2Y6",
-                "(604) 854-4397",
-                "julieallow@gmail.com",
-                "",
-                "June 3, 2021",
-                "The hospital",
-                Contact.Category.OTHER);
-        testContactList.addContact("",
-                "56 Singer Crescent, Canmore, AB, R3E 8I4",
-                "(403) 223-3421",
-                "hair@thehairplace.com",
-                "",
-                "March 27, 2021",
-                "",
-                Contact.Category.OTHER);
-        assertEquals("\n" + "Full name: " + "Pete Hamilton" + "\n" +
-                "Address: " + "21 Throllalach Way, Vancouver, BC, N2D 7H1" + "\n" +
-                "Phone number: " + "(403) 893-2343" + "\n" +
-                "Email: " + "peteham@hotmail.com" + "\n" +
-                "Birthday: " + "July 7, 1970" + "\n" +
-                "Date added: " + "October 16, 2007" + "\n" +
-                "Location met: " + "" + "\n" +
-                "Category: " + Contact.Category.FAMILY + "\n" +
-                "-------------------" +
-                "\n" + "Full name: " + "Molly Brown" + "\n" +
-                "Address: " + "178 Turner Road, Vancouver, BC, V8L 9E2" + "\n" +
-                "Phone number: " + "(604) 902-3789" + "\n" +
-                "Email: " + "mollybrown@gmail.com" + "\n" +
-                "Birthday: " + "January 9, 2000" + "\n" +
-                "Date added: " + "July 21, 2022" + "\n" +
-                "Location met: " + "Walter Gage" + "\n" +
-                "Category: " + Contact.Category.FRIEND + "\n" +
-                "-------------------" +
-                "\n" + "Full name: " + "Bob Li" + "\n" +
-                "Address: " + "1345 Blankar Terrace, Edmonton, AB, H7T 9P4" + "\n" +
-                "Phone number: " + "(403) 124-6784" + "\n" +
-                "Email: " + "bobli@gmail.com" + "\n" +
-                "Birthday: " + "November 16, 1999" + "\n" +
-                "Date added: " + "May 1, 2022" + "\n" +
-                "Location met: " + "Business conference" + "\n" +
-                "Category: " + Contact.Category.WORK + "\n" +
-                "-------------------" +
-                "\n" + "Full name: " + "Julie Allo" + "\n" +
-                "Address: " + "4 Ruffert Street, Kelowna, BC, A5T 2Y6" + "\n" +
-                "Phone number: " + "(604) 854-4397" + "\n" +
-                "Email: " + "julieallow@gmail.com" + "\n" +
-                "Birthday: " + "" + "\n" +
-                "Date added: " + "June 3, 2021" + "\n" +
-                "Location met: " + "The hospital" + "\n" +
-                "Category: " + Contact.Category.OTHER + "\n" +
-                "-------------------" +
-                "\n" + "Full name: " + "" + "\n" +
-                "Address: " + "56 Singer Crescent, Canmore, AB, R3E 8I4" + "\n" +
-                "Phone number: " + "(403) 223-3421" + "\n" +
-                "Email: " + "hair@thehairplace.com" + "\n" +
-                "Birthday: " + "" + "\n" +
-                "Date added: " + "March 27, 2021" + "\n" +
-                "Location met: " + "" + "\n" +
-                "Category: " + Contact.Category.OTHER + "\n" +
-                "-------------------",
-                testContactList.listOfContactsToString());
-    }
-
-    @Test
-    public void testListOfContactsToStringOutOfOrder() {
-        testContactList.addContact("Molly Brown",
-                "178 Turner Road, Vancouver, BC, V8L 9E2",
-                "(604) 902-3789",
-                "mollybrown@gmail.com",
-                "January 9, 2000",
-                "July 21, 2022",
-                "Walter Gage",
-                Contact.Category.FRIEND);
-        testContactList.addContact("Julie Allo",
-                "4 Ruffert Street, Kelowna, BC, A5T 2Y6",
-                "(604) 854-4397",
-                "julieallow@gmail.com",
-                "",
-                "June 3, 2021",
-                "The hospital",
-                Contact.Category.OTHER);
-        testContactList.addContact("",
-                "56 Singer Crescent, Canmore, AB, R3E 8I4",
-                "(403) 223-3421",
-                "hair@thehairplace.com",
-                "",
-                "March 27, 2021",
-                "",
-                Contact.Category.OTHER);
-        testContactList.addContact("Pete Hamilton",
-                "21 Throllalach Way, Vancouver, BC, N2D 7H1",
-                "(403) 893-2343",
-                "peteham@hotmail.com",
-                "July 7, 1970",
-                "October 16, 2007",
-                "",
-                Contact.Category.FAMILY);
-        testContactList.addContact("Bob Li",
-                "1345 Blankar Terrace, Edmonton, AB, H7T 9P4",
-                "(403) 124-6784",
-                "bobli@gmail.com",
-                "November 16, 1999",
-                "May 1, 2022",
-                "Business conference",
-                Contact.Category.WORK);
-        assertEquals("\n" + "Full name: " + "Pete Hamilton" + "\n" +
-                        "Address: " + "21 Throllalach Way, Vancouver, BC, N2D 7H1" + "\n" +
-                        "Phone number: " + "(403) 893-2343" + "\n" +
-                        "Email: " + "peteham@hotmail.com" + "\n" +
-                        "Birthday: " + "July 7, 1970" + "\n" +
-                        "Date added: " + "October 16, 2007" + "\n" +
-                        "Location met: " + "" + "\n" +
-                        "Category: " + Contact.Category.FAMILY + "\n" +
-                        "-------------------" +
-                        "\n" + "Full name: " + "Molly Brown" + "\n" +
-                        "Address: " + "178 Turner Road, Vancouver, BC, V8L 9E2" + "\n" +
-                        "Phone number: " + "(604) 902-3789" + "\n" +
-                        "Email: " + "mollybrown@gmail.com" + "\n" +
-                        "Birthday: " + "January 9, 2000" + "\n" +
-                        "Date added: " + "July 21, 2022" + "\n" +
-                        "Location met: " + "Walter Gage" + "\n" +
-                        "Category: " + Contact.Category.FRIEND + "\n" +
-                        "-------------------" +
-                        "\n" + "Full name: " + "Bob Li" + "\n" +
-                        "Address: " + "1345 Blankar Terrace, Edmonton, AB, H7T 9P4" + "\n" +
-                        "Phone number: " + "(403) 124-6784" + "\n" +
-                        "Email: " + "bobli@gmail.com" + "\n" +
-                        "Birthday: " + "November 16, 1999" + "\n" +
-                        "Date added: " + "May 1, 2022" + "\n" +
-                        "Location met: " + "Business conference" + "\n" +
-                        "Category: " + Contact.Category.WORK + "\n" +
-                        "-------------------" +
-                        "\n" + "Full name: " + "Julie Allo" + "\n" +
-                        "Address: " + "4 Ruffert Street, Kelowna, BC, A5T 2Y6" + "\n" +
-                        "Phone number: " + "(604) 854-4397" + "\n" +
-                        "Email: " + "julieallow@gmail.com" + "\n" +
-                        "Birthday: " + "" + "\n" +
-                        "Date added: " + "June 3, 2021" + "\n" +
-                        "Location met: " + "The hospital" + "\n" +
-                        "Category: " + Contact.Category.OTHER + "\n" +
-                        "-------------------" +
-                        "\n" + "Full name: " + "" + "\n" +
-                        "Address: " + "56 Singer Crescent, Canmore, AB, R3E 8I4" + "\n" +
-                        "Phone number: " + "(403) 223-3421" + "\n" +
-                        "Email: " + "hair@thehairplace.com" + "\n" +
-                        "Birthday: " + "" + "\n" +
-                        "Date added: " + "March 27, 2021" + "\n" +
-                        "Location met: " + "" + "\n" +
-                        "Category: " + Contact.Category.OTHER + "\n" +
-                        "-------------------",
-                testContactList.listOfContactsToString());
     }
 }
