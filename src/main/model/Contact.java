4de5976a102ef2c0fbd69cb7ac9172908cda0de1
jenessagordon <68665601+jenessagordon@users.noreply.github.com>
2022-07-31 20:13:51 -0700

    Phase 2 - Added Writable interface. Added toJson() method in Contact

--- src/main/model/Contact.java
+++ src/main/model/Contact.java
@@ -1,9 +1,12 @@
 package model;
 
+import org.json.JSONObject;
+import persistence.Writeable;
+
 // Represents a contact containing the contact’s full name, address, phone number, email, and birthday, along with the
 // date the user added the contact to the contact list, where the user met the contact, and what Category the contact
 // is in. Category represents the type of the contact, and is one of: FAMILY, FRIEND, WORK, or OTHER.
-public class Contact {
+public class Contact implements Writeable {
     private String fullName;
     private String address;
     private String phoneNum;
@@ -66,5 +69,19 @@ public class Contact {
 
     public Category getCategory() {
         return category;
+    }
+
+    @Override
+    public JSONObject toJson() {
+        JSONObject json = new JSONObject();
+        json.put("fullName", fullName);
+        json.put("address", address);
+        json.put("phoneNum", phoneNum);
+        json.put("email", email);
+        json.put("birthday", birthday);
+        json.put("dateAdded", dateAdded);
+        json.put("locationMet", locationMet);
+        json.put("category", category);
+        return json;
     }
 }
